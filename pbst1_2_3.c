ee13c35d2cca8de07946c5499d6367c05c4fadeb
Sarat-Raj <sidharthbatchu@gmail.com>
2021-12-05 23:47:38 +0530

    solved the logic for pbst2

--- pbst1_2_3.c
+++ pbst1_2_3.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include <stdlib.h>
 float PI = 3.14159, angle_formed[25], sensor1Data[25], sensor2Data[25], a1[25], a2 = 30.0, current_distance_from_wall[25], cm_per_2k_rev = 15.708;
-int loop = 0,  row = 0, column = 0, sD1 = 0, sD2 = 0, rev_per_rotation = 2000, rota_per_turn = 180, m1[25], m2[25], m3[25], m4[25], rev[25], bot_dir1[25], bot_dir2[25]; 
+int loop = 0,  row = 0, column = 0, sD1 = 0, sD2 = 0, rev_per_rotation = 2000, rota_per_turn = 180, m1[25], m2[25], m3[25], m4[25], steps[25], bot_dir1[25], bot_dir2[25], dist_from_wall = 30; 
 // a1 is the difference between two sensor data (opp side of the angle)
 // a2 is the distance betweeen the two sensors (adj side of the angle)
 // assuming that one revolution of the wheel, there is a 180° turning
@@ -44,7 +44,7 @@ void pbst1()
     {
         a1[loop] = sensor1Data[loop]-sensor2Data[loop];
         angle_formed[loop] = (atan(a1[loop]/a2) * 180)/PI;
-        printf("\nangle_formed[%d]:%f",loop+1,angle_formed[loop]);
+        //printf("\nangle_formed[%d]:%f",loop+1,angle_formed[loop]);
     }
 }
 
@@ -52,14 +52,16 @@ void pbst2()
 {
     for (loop = 0; loop < row; loop++)
     {
-        if (angle_formed[loop] > 0) bot_dir1[loop]=1;//clockwise
-        if (angle_formed[loop] < 0) bot_dir1[loop]=0;//anticlockwise
-        if (angle_formed[loop] == 0) {rev[loop] = 0; bot_dir1[loop] = 1;}
-        rev[loop] = (int)abs((rev_per_rotation*angle_formed[loop])/rota_per_turn);
-        //instead just doing this, a better approach would be as one rev of motor = 10pi cms, radius = 30 cms, cal angle based on that value
-        // but opting this metod as its quicker
-        m3[loop]=rev[loop];
-        m4[loop]=rev[loop];
+        // as one rev of motor = 5pi cms, radius = 30 cms, caliculating angle based on that value,
+        // 60pi=188.5 
+        // => 189/360 = x/angle_formed[loop] , where x = displacement required by the wheel
+        // =>  x = 189*angle_formed[loop]/360
+        // considering either m3/m4 will move while the rest of the wheels are static causes
+        // a pivot force with center as the other sensor
+        steps[loop] = (int)((189*angle_formed[loop]*rev_per_rotation)/360);
+        if (steps[loop] > 0) {bot_dir1[loop] = 0; m3[loop] = steps[loop]; m4[loop] = 0;}
+        if (steps[loop] < 0) {bot_dir1[loop] = 1; m3[loop] = 0; m4[loop] = steps[loop];}
+        if (steps[loop] == 0) {bot_dir1[loop] = 1; m3[loop] =0; m4[loop] = 0;}
         printf("\n rev[%d] = %d : direction = %d",loop,m3[loop],bot_dir1[loop]);
     }
 
@@ -70,26 +72,26 @@ void pbst3()
     for (loop = 0; loop < row; loop++)
     {
         current_distance_from_wall[loop] = abs(a1[loop]);
-        if (current_distance_from_wall[loop] > 30) 
+        if (current_distance_from_wall[loop] > dist_from_wall) 
         {
-            m1[loop] = (int)((current_distance_from_wall[loop]-30)*2000)/cm_per_2k_rev; 
+            m1[loop] = (int)((current_distance_from_wall[loop]-dist_from_wall)*rev_per_rotation)/cm_per_2k_rev; 
             m2[loop] = m1[loop]; 
             bot_dir2[loop]=1;
         }
-        if (current_distance_from_wall[loop] < 30) 
+        if (current_distance_from_wall[loop] < dist_from_wall) 
         { 
-            m1[loop] = (int)((30 - current_distance_from_wall[loop])*2000)/cm_per_2k_rev; 
+            m1[loop] = (int)((dist_from_wall - current_distance_from_wall[loop])*rev_per_rotation)/cm_per_2k_rev; 
             m2[loop] = m1[loop]; 
             bot_dir2[loop]=0;
         }
-        if (current_distance_from_wall[loop] == 30) 
+        if (current_distance_from_wall[loop] == dist_from_wall) 
         {
             m1[loop] = 0; 
             m2[loop] = m1[loop]; 
             bot_dir2[loop]=1;
         }
-        printf("\ncurrent_distance_from_wall[%d]:%f",loop,current_distance_from_wall[loop]);
-        printf("\nm1:%d\t,m2:%d\t,botdir2[loop]:%d\t",m1[loop],m2[loop],bot_dir2[loop]);
+        //printf("\ncurrent_distance_from_wall[%d]:%f",loop,current_distance_from_wall[loop]);
+        //printf("\nm1:%d\t,m2:%d\t,botdir2[loop]:%d\t",m1[loop],m2[loop],bot_dir2[loop]);
     }
 }
 
