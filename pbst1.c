3ba7983eb006c5ad2c84c4666f05a258ee52ed3c
Sarat-Raj <sidharthbatchu@gmail.com>
2021-12-05 19:50:41 +0530

    working code, output.csv

--- pbst1.c
+++ pbst1.c
@@ -12,51 +12,49 @@ void main()
 // a+ = a + new data is written at the end (Both reading and writing)
 // fscanf = 
 // fgets = 
-    int loop = 0;
-    float angle_formed[25];
-    FILE *fp = fopen("input - Sheet1(1).csv", "r");
-    if(fp == NULL){printf("unable to open file");exit(1);}
+    int loop = 0,  row = 0, column = 0, sD1 = 0, sD2 = 0;
+    float angle_formed[25], sensor1Data[25], sensor2Data[25];
+    FILE *file_sensor_data = fopen("input - Sheet1(1).csv", "r");
+    FILE *file_output_data;
+    if(file_sensor_data == NULL){printf("unable to open file");exit(1);}
     char buffer[1024];
-    float sensor1Data[25], sensor2Data[25];
-    int row = 0, column = 0, sD1 = 0, sD2 = 0;
     // a1 is the difference between two sensor data (opp side of the angle)
     // a2 is the distance betweeen the two sensors (adj side of the angle)
     float a1, a2 = 30.0;
-    for(row = 0; fgets(buffer, sizeof(buffer), fp); sD1++, sD2++, row++)
+    for(row = 0; fgets(buffer, sizeof(buffer), file_sensor_data); sD1++, sD2++, row++)
     {
         char *value = strtok(buffer, ", ");
         for (column = 0; value; column++)
         {
+            //sensor 1
             if (column == 0) 
             {
-                //s1 = sensor 1
                 printf("S1 : %s", value);
                 //I wanted to convert that value into a float
                 sensor1Data[sD1] = (float)strtod(value, NULL);
             }
+            //sensor 2
             if (column == 1)
             {
-                //s2 = sensor 2
-                printf("\tS2 :%s", value);
                 //I wanted to convert that value into a float
                 sensor2Data[sD2] = (float)strtod(value, NULL);
             }
             value = strtok(NULL, ", ");
         }
-        printf(" row: %d \n",row);
     }
-    fclose(fp);
+    fclose(file_sensor_data);
     for(loop = 0; loop < row; loop++)
     {
         a1 = sensor1Data[loop]-sensor2Data[loop];
         angle_formed[loop] = (atan(a1/a2) * 180)/PI;
-        printf("\n for Sensor1Data: %.2f , Sensor2Data: %.2f ,angle : %.2f",sensor1Data[loop],sensor2Data[loop],angle_formed[loop]);
-        printf("\n");
     }
+    file_output_data = fopen("output_data.csv", "a");
+    if (!file_output_data) { printf("Error creating/ opening line"); exit(1); }    
     for(loop=0; loop < row; loop++)
     {
         if (angle_formed[loop] < 0) angle_formed[loop]=180.0-angle_formed[loop];
-        printf("corrected angle formed %f \n",angle_formed[loop]);
+        //printf("corrected angle formed %f \n",angle_formed[loop]);
+        fprintf(file_output_data,"%f,",angle_formed[loop]);
     }
 }
  
